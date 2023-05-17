4c250dea25ade58aaa8d0459a687134ca094f576
Ramil Dzhafarov <rdb03@mail.ru>
2023-05-17 16:34:44 +0500

    Добавил контейнеры и кнопку в разметку страницы  Прописал логику появления рандомных чисел  Добавил функцию для перезаписи данных в контейнерах

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Person from "./Person/Person";
 import './App.css';
 
-const App = () => (
-    <div className="App">
+const App = () => {
+   const [number, setRandom] = useState([
+       {number: 0},
+       {number: 0},
+       {number: 0},
+       {number: 0},
+       {number: 0},
+   ]);
 
-    </div>
-);
+    const case1 = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    const case2 = Math.floor(Math.random() * (15 - 11 + 1)) + 11;
+    const case3 = Math.floor(Math.random() * (22 - 16 + 1)) + 16;
+    const case4 = Math.floor(Math.random() * (28 - 23 + 1)) + 23;
+    const case5 = Math.floor(Math.random() * (32 - 29 + 1)) + 29;
+
+
+    const changeNumber = () => {
+        setRandom([
+            {number: case1},
+            {number: case2},
+            {number: case3},
+            {number: case4},
+            {number: case5},
+        ])
+    }
+
+    return(
+        <div className="App">
+            <div className="numberDiv">
+                <Person number={number[0].number}></Person>
+                <Person number={number[1].number}></Person>
+                <Person number={number[2].number}></Person>
+                <Person number={number[3].number}></Person>
+                <Person number={number[4].number}></Person>
+            </div>
+            <div>
+                <button className="numberBtn" onClick={changeNumber}>Change number</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
